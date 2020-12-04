818dcd3af8c184a25a877ff7ecab0c84c7166492
Arterius <zaghidullinr@bk.ru>
2020-12-04 21:37:07 +0500

    Практическое задание № 5 (2)

--- script_5.js
+++ script_5.js
@@ -9,6 +9,12 @@ function Chess() {
     let mainBlock = document.querySelector('.main-block');
     let block;
     let flag = true;
+    let figure = { //создаем массив с расположением фигурам
+        0: ['-263px -19px', '-484px -22px', '-372px -17px', '-150px -16px', '-38px -16px', '-372px -17px', '-484px -22px', '-263px -19px'],
+        1: ['-595px -19px', '-595px -19px', '-595px -19px', '-595px -19px', '-595px -19px', '-595px -19px', '-595px -19px', '-595px -19px'],
+        6: ['-595px -116px', '-595px -116px', '-595px -116px', '-595px -116px', '-595px -116px', '-595px -116px', '-595px -116px', '-595px -116px'],
+        7: ['-263px -116px', '-484px -116px', '-372px -116px', '-38px -116px', '-150px -116px', '-372px -116px', '-484px -116px', '-263px -116px'],
+    };
     for (let i = 0; i < 8; i++) {
         for (let j = 0; j < 8; j++) {
             if (j == 0) {
@@ -19,6 +25,10 @@ function Chess() {
                 block.className = 'block black';
             } else {
                 block.className = 'block white';
+            }
+            if (figure[i] !== undefined && figure[i][j] !== undefined) {
+                block.style.backgroundImage = 'url(img/сhess_symbols.png)';
+                block.style.backgroundPosition = figure[i][j];
             }
             mainBlock.appendChild(block);
             flag = !flag;
